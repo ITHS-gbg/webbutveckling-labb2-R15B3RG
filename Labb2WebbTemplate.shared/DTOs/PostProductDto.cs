6c452f53f6570906cf2fb230807038463b290595
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-03-21 14:26:47 +0100

    Push

---
+++ Labb2WebbTemplate.shared/DTOs/PostProductDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2WebbTemplate.shared.DTOs
+{
+    public class PostProductDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string ProductType { get; set; }
+
+        public int Inventory { get; set; }
+
+        public bool InventoryBalance { get; set; }
+    }
+}
